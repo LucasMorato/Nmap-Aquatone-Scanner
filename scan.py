b42aec6e09919366c0b6c60c5d5680d4b0525f99
LucasMorato <lmorato@gmail.com>
2023-05-19 04:45:24 -0300

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,15 +1,20 @@
 import argparse
 import os
+import shutil
 
 def run_aquatone(site, flag):
     nmap_command = f"nmap {'-' if flag[0] != '-' else ''}{flag} -oX {site}.xml {site}"
     cat_command = f"cat {site}.xml | aquatone"
+    create_folder_command = f"mkdir {site}"
 
     # Executa o comando nmap com a flag -oX e a flag fornecida pelo usuário
     os.system(nmap_command)
 
     # Executa o comando cat com o pipe para o comando aquatone
     os.system(cat_command)
+
+    # Cria uma pasta com o nome do site pesquisado
+    os.system(create_folder_command)
 
 if __name__ == "__main__":
     # Cria um parser de argumentos
@@ -30,5 +35,18 @@ if __name__ == "__main__":
     # Verifica se o argumento --site foi fornecido
     if args.site:
         run_aquatone(args.site, flag)
+
+        # Cria uma pasta com o nome do site pesquisado
+        os.makedirs(args.site, exist_ok=True)
+
+        # Obtém o caminho atual do diretório
+        current_directory = os.getcwd()
+
+        # Move todos os arquivos e pastas, exceto o script, para a pasta gerada
+        for item in os.listdir(current_directory):
+            if item != os.path.basename(__file__) and item != args.site:
+                item_path = os.path.join(current_directory, item)
+                destination_path = os.path.join(current_directory, args.site, item)
+                shutil.move(item_path, destination_path)
     else:
         print("É necessário fornecer o nome do site através do argumento --site.")
